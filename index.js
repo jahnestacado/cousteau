89b51f72a9041a33f140b7637774ee4ba36f5874
Jahn <ioannis.kiwe@gmail.com>
2017-02-11 12:43:18 +0100

    index.js: Added backwards compatibility support

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require("./lib/uberfind.js");
+var majorNodeVersion = parseInt(process.version.match(/v(\d)+/)[1]);
+if( majorNodeVersion < 6) {
+    module.exports = require("./lib-gen/uberfind.js");
+} else {
+    module.exports = require("./lib/uberfind.js");
+}
